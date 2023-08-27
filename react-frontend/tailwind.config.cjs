7ee0e84b821c773b9b244961efbdaed84781585f
skmirajbn <skmirajbn@gmail.com>
2023-08-28 00:07:49 +0600

    updated

--- react-frontend/tailwind.config.cjs
+++ react-frontend/tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
