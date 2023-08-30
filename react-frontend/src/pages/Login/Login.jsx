3012d6fce679d8cf49c0ae4db8257c05baca4521
skmirajbn <skmirajbn@gmail.com>
2023-08-30 12:03:16 +0600

    updated

---
+++ react-frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,43 @@
+import Button from "./../../components/Button";
+import MotionWrapScale from "./../../components/motionWrap/MotionWrapScale";
+export default function Login() {
+  return (
+    <div className="bg-blue-300">
+      <div className="container">
+        <form>
+          <section id="web-job-category" className="">
+            <MotionWrapScale>
+              <div className="bg-[url('./assets/img/bg-doddle.jpg')] md:bg-[21%_auto] bg-[50%_auto] py-6 px-12 rounded-md  h-[82vh] flex justify-center items-center">
+                <div className="h-[420px] w-96 bg-green-100 rounded-lg py-10 px-10 space-y-6 shadow-xl">
+                  <h3 className="text-2xl text-center font-bold">Sign In</h3>
+
+                  <div className="space-y-2">
+                    <h4 className="text-md">Username</h4>
+                    <input className="w-full h-10 rounded-md px-4 border border-green-900" type="text" placeholder="Enter Your Username" />
+                  </div>
+
+                  <div className="space-y-2">
+                    <h4 className="text-md">Password</h4>
+                    <input className="w-full h-10 rounded-md px-4 border border-green-900" type="text" placeholder="Enter Your Password" />
+                  </div>
+
+                  <div className="">
+                    <Button className="mx-auto block">
+                      <a className="bg-blue-500 px-6 py-1 rounded-full text-white">Login</a>
+                    </Button>
+                  </div>
+
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-gray-600">Forgot Password?</h3>
+
+                    <a className="bg-orange-600 text-white px-4 py-1 rounded-full">Create Account</a>
+                  </div>
+                </div>
+              </div>
+            </MotionWrapScale>
+          </section>
+        </form>
+      </div>
+    </div>
+  );
+}
--- react-frontend/src/router.jsx
+++ react-frontend/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Page404 from "./pages/Page404";
 
 const router = createBrowserRouter([
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
