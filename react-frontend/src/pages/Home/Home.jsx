9414485b5dfc2cb59d9ea47d99ad8d81294ed188
skmirajbn <skmirajbn@gmail.com>
2023-08-30 10:17:41 +0600

    updated

--- react-frontend/src/pages/Home/Home.jsx
+++ react-frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Accommodation from "./Accommodation";
 import Banner from "./Banner";
+import BedsWeProvide from "./BedsWeProvide";
 import OurBranches from "./OurBranches";
 import Services from "./Services";
 
@@ -11,6 +12,7 @@ export default function Home() {
         <Services />
         <Accommodation />
         <OurBranches />
+        <BedsWeProvide />
       </div>
     </>
   );
