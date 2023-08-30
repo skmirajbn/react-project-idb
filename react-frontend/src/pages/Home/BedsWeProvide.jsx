61c46c5b8c1da0783b3f8abea072b81ad2561e82
skmirajbn <skmirajbn@gmail.com>
2023-08-30 10:21:54 +0600

    updated

--- react-frontend/src/pages/Home/BedsWeProvide.jsx
+++ react-frontend/src/pages/Home/BedsWeProvide.jsx
@@ -1,3 +1,20 @@
+import Heading from "../../components/Heading";
+
 export default function BedsWeProvide() {
-  return <div>BedsWeProvide</div>;
+  return (
+    <div className="container">
+      <div className="py-20 space-y-10">
+        <Heading>Accommodation</Heading>
+        <section>
+          <div>
+            <h3>Heading</h3>
+            <p>Description</p>
+          </div>
+          <div>
+            <img src="src\assets\img\room-1.jpg" alt="" />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
