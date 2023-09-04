f7ea982ff9ae7d03fd4e2e2b903a2236e3ce4ad1
skmirajbn <skmirajbn@gmail.com>
2023-09-04 12:28:55 +0600

    updated

--- react-frontend/src/pages/Home/ContactSectin.jsx
+++ react-frontend/src/pages/Home/ContactSectin.jsx
@@ -5,6 +5,18 @@ function ContactSectin() {
     <div className="container">
       <div className="py-20 space-y-10">
         <Heading>Contact Information</Heading>
+        <div className="flex">
+          <div>
+            <h3>Send Message</h3>
+            <form className="flex flex-col">
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Mobile Number" />
+              <input type="text" placeholder="Email ID" />
+              <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+            </form>
+          </div>
+          <div>contact</div>
+        </div>
       </div>
     </div>
   );
