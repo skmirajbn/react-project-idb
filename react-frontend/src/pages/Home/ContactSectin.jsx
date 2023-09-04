9dfe47bc22238c36e28d044b07ce969b8ac76267
skmirajbn <skmirajbn@gmail.com>
2023-09-04 18:46:42 +0600

    updated

--- react-frontend/src/pages/Home/ContactSectin.jsx
+++ react-frontend/src/pages/Home/ContactSectin.jsx
@@ -5,14 +5,14 @@ function ContactSectin() {
     <div className="container">
       <div className="py-20 space-y-10">
         <Heading>Contact Information</Heading>
-        <div className="flex gap-10">
+        <div className="flex gap-10 ">
           <div className="w-1/2 space-y-6">
             <h3 className="text-center text-xl font-bold text-gray-700">Send Message</h3>
             <form className="flex flex-col space-y-4">
               <input className="bg-white border-2 border-black rounded-md px-4 py-2" type="text" placeholder="Name" />
               <input className="bg-white border-2 border-black rounded-md px-4 py-2" type="text" placeholder="Mobile Number" />
               <input className="bg-white border-2 border-black rounded-md px-4 py-2" type="text" placeholder="Email ID" />
-              <textarea className="bg-white border-2 border-black rounded-md px-4 py-2" name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+              <textarea className="bg-white border-2 border-black rounded-md px-4 py-2" name="" id="" cols="30" rows="7" placeholder="Message"></textarea>
               <div className="mx-auto">
                 <button className="bg-blue-600 text-white py-2 rounded-md px-6" type="submit">
                   Send <i className="fa-solid fa-paper-plane pl-2"></i>
@@ -20,9 +20,9 @@ function ContactSectin() {
               </div>
             </form>
           </div>
-          <div className="w-1/2">
+          <div className="w-1/2 space-y-6">
             <h3 className="text-center text-xl font-bold text-gray-700">Contact Information</h3>
-            <div className="bg-gray-700 text-white h-full rounded-xl p-8 space-y-10">
+            <div className="bg-gray-700 text-white rounded-xl p-10 space-y-10 ">
               <div className="space-y-6">
                 <h3 className="text-2xl text-center">Our Contact Information</h3>
                 <div className="space-y-3">
