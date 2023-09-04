d2b6911ea647562ff5a4ff277fc2b0b8aba933f2
skmirajbn <skmirajbn@gmail.com>
2023-09-04 08:53:31 +0600

    updated

--- react-frontend/src/pages/Home/Home.jsx
+++ react-frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Accommodation from "./Accommodation";
 import Banner from "./Banner";
 import BedsWeProvide from "./BedsWeProvide";
+import MediaCoverage from "./MediaCoverage";
 import OurBranches from "./OurBranches";
 import Services from "./Services";
 
@@ -13,6 +14,7 @@ export default function Home() {
         <Accommodation />
         <OurBranches />
         <BedsWeProvide />
+        <MediaCoverage />
       </div>
     </>
   );
---
+++ react-frontend/src/pages/Home/MediaCoverage.jsx
@@ -0,0 +1,20 @@
+import Heading from "../../components/Heading";
+
+function MediaCoverage() {
+  return (
+    <div className="container">
+      <div className="py-20 space-y-10">
+        <Heading>On Top Media Coverage</Heading>
+        <div>
+          {/* Single Coverage Media */}
+          <div>
+            <img src="/img/media-coverage/prothom-alo.webp" alt="" />
+            <h3>Prothom Alo</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MediaCoverage;
