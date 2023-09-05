e480816f793b780549790deb377942c8613b7ea7
skmirajbn <skmirajbn@gmail.com>
2023-09-05 10:49:12 +0600

    updated

--- react-frontend/src/pages/BookSeat/BookSeat.jsx
+++ react-frontend/src/pages/BookSeat/BookSeat.jsx
@@ -1,8 +1,109 @@
+import Button from "../../components/Button";
+import MotionWrapScale from "../../components/motionWrap/MotionWrapScale";
+
+/* eslint-disable react/no-unescaped-entities */
 function BookSeat() {
   return (
-    <div className="">
-      <div className="container">
+    <div className="py-10">
+      <div className="container space-y-6">
         <h3 className="text-center text-2xl font-bold">Book Seat</h3>
+        <h3 className="text-xl font-medium">
+          <i className="fa-solid fa-house-circle-check"></i> Select Branch
+        </h3>
+        <div>
+          <div className="grid grid-cols-3 items-center gap-6">
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-1.png" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Dhanmondi Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-2.jpg" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Motijheel Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-3.jpg" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Mohammadpur Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-4.jpg" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Mirpur Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-5.jpg" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Banani Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+            {/* Single Branch */}
+            <MotionWrapScale>
+              <div className="space-y-2">
+                <img className="w-full h-64 rounded-xl" src="/img/hostel-6.jpg" alt="" />
+                <h3 className="text-2xl text-center font-semibold">Uttara Branch</h3>
+                <h4 className="text-center text-gray-500">
+                  <i className="fa-solid fa-location-dot"></i> Momotaz Plaza, Dhanmondi, Dhaka
+                </h4>
+                <div className="w-full flex justify-center">
+                  <Button className="bg-blue-500 px-6 leading-8 text-white rounded-full">
+                    Select Branch <i className="fa-solid fa-chevron-right"></i>
+                  </Button>
+                </div>
+              </div>
+            </MotionWrapScale>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ react-frontend/src/pages/BookSeat/BookSeatForm.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/no-unescaped-entities */
+function BookSeatForm() {
+  return (
+    <form>
+      {/* User info For signup */}
+      <div>
+        <div>
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Phone</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">National ID</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Person/Person's</label>
+          <input type="number" />
+        </div>
+      </div>
+      {/* Booking */}
+      <div>
+        <div>
+          <label htmlFor="">Pakage</label>
+          <select name="" id="">
+            <option value="">Select Pakage</option>
+            <option value="">3 days</option>
+            <option value="">7 days</option>
+            <option value="">30 days</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">Branch</label>
+          <select name="" id="">
+            <option value="">Select Branch</option>
+            <option value="">Dhanmondi</option>
+            <option value="">Motijheel</option>
+            <option value="">Uttara</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">Room</label>
+          <select name="" id="">
+            <option value="">Select availabe Room</option>
+            <option value="">3A</option>
+            <option value="">4A</option>
+            <option value="">5A</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">Bed</label>
+          <select name="" id="">
+            <option value="">Select availabe Bed</option>
+            <option value="">Bed-2</option>
+            <option value="">Bed-3</option>
+            <option value="">Bed-4</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default BookSeatForm;
